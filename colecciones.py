304fbf7ffc3c5e70f2bf3f5ac4a54896d9eb7c5f
Mateox1011 <cristiancaicedocanelo@gmail.com>
2026-05-29 17:17:00 -0500

    Update colecciones.py

--- colecciones.py
+++ colecciones.py
@@ -1,9 +1,9 @@
 frutas=["manzana","banana","naranja"]
 print(frutas[0])
 print(frutas[-1])
-frutas.append("uva")
-frutas.insert(1,"pera")
-frutas.remove("banana")
-ultimo=frutas.pop()
+frutas.append("uva")#agrega un elemento al final de la lista
+frutas.insert(1,"pera") #agrega un elemento en la posición especificada, desplazando los elementos posteriores hacia la derecha
+frutas.remove("banana")    #elimina el primer elemento que coincida con el valor dado
+ultimo=frutas.pop() #elimina el ultimo elemento de la lista y lo devuelve
 print("elemento eliminado:",ultimo)
 del frutas[0]
