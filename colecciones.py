aa5dfefc77abd084149cbd78d5e86a7e8a1eb7a9
Mateox1011 <cristiancaicedocanelo@gmail.com>
2026-05-29 17:15:26 -0500

    Add files via upload

---
+++ colecciones.py
@@ -0,0 +1,9 @@
+frutas=["manzana","banana","naranja"]
+print(frutas[0])
+print(frutas[-1])
+frutas.append("uva")
+frutas.insert(1,"pera")
+frutas.remove("banana")
+ultimo=frutas.pop()
+print("elemento eliminado:",ultimo)
+del frutas[0]
